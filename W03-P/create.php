5a26cc15a4eede23b1e6a8596efc90f2d2807fea
ksyyeon <69140802+ksyyeon@users.noreply.github.com>
2020-09-21 13:01:00 +0900

    W03-P

---
+++ W03-P/create.php
@@ -0,0 +1,41 @@
+<?php
+  $link = mysqli_connect('localhost', 'root', '20172026', 'dbp');
+  $query = "SELECT * FROM color";
+  $result = mysqli_query($link, $query);
+  $list = '';
+  while($row = mysqli_fetch_array($result)){
+    $list = $list."<li><a href =\"index.php?id={$row['id']}\">{$row['name']}</a></li>";
+  }
+
+  $article = array (
+    'code' => 'FFFFFF',
+    'name' => 'White'
+  );
+
+  if( isset($_GET['id']) ){
+    $query = "SELECT * FROM color WHERE id={$_GET['id']}";
+    $result = mysqli_query($link, $query);
+    $row = mysqli_fetch_array($result);
+    $article = array(
+        'code' => $row['code'],
+        'name' => $row['name']
+    );
+  }
+ ?>
+
+ <!DOCTYPE html>
+ <html>
+ <head>
+   <meta charset="utf-8">
+   <title> Color Palette </title>
+ </head>
+ <body bgcolor = <?= $article['code'] ?>>
+   <h1> <a href = "index.php"> Color Palette </a> </h1>
+   <h3> click the name and take a look at color you like! </h3>
+   <ol> <?= $list ?> </ol>
+   <form action="process_create.php" method ="post">
+     <p> HEX <input type="text" name ="code" placeholder="ex) FFFFFF"></p>
+     <p> Name <input type="text" name ="name" placeholder="ex) White"></p>
+     <p><input type="submit"></p>
+</body>
+</html>
